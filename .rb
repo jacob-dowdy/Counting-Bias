bf5eb09d72de3e31a8e8ae0feccfdbd7afef5004
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-02-17 23:51:28 -0500

    Update .rb

--- .rb
+++ .rb
@@ -1,5 +1,5 @@
 puts 'Hi! What\'s your favorite number?'
 num = gets.chomp
-new_num = num.to_i + 1
+new_num = num.to_i + 2
 puts num + '? ' + new_num.to_s + ' is a much better number!'
 puts 'Do you even count, bro?'
